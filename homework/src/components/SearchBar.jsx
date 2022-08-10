0168da5f7984f2dc826971f34b7e63b84c70067e
Silvina Vildoza <89544485+SilVildoza@users.noreply.github.com>
2022-08-09 22:42:28 -0300

    Add files via upload

---
+++ homework/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchBar({onSearch}) {
+  // acá va tu código
+  return (
+  <div>
+    <input type="search" placeholder='Ciudad...'/>
+    <button onClick={()=>onSearch("Buscando...")}>Agregar</button>
+  </div>
+  );
+};
